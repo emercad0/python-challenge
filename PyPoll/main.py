671254f2aa1079dadc9c5618ac30e23db39075a5
emercad049 <emercad049@gmail.com>
2018-09-23 20:28:18 -0400

    Code Release - including updates to the pyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,74 +1,163 @@
 import os
 import csv
-from collections import defaultdict
 
-
-
-data_file = os.path.join("Resources","budget_data_1.csv")
-data_file2 = os.path.join("Resources","budget_data_2.csv")
+data_file = os.path.join("Resources","election_data_2.csv")
 r_analysis = os.path.join("Resources","results.txt")
 
-#!ls
-#pwd
 
-#The total number of months included in the dataset
-# Add up all the months and if the date is equal to each other don't count 
-#The total amount of revenue gained over the entire period
-# Sum of column 2 
-#The average change in revenue between months over the entire period
-# average of totalSum - 
-#The greatest increase in revenue (date and amount) over the entire period
-#The greatest decrease in revenue (date and amount) over the entire period
-
-
-ttlMonths = 0 
-prevRevenue = 0 
-month0fChng = []
-revDiff = []
-greatestInc = ["", 0]
-greatestDec = ["", 999999]
-amntOfRev = 0 
-avgRev = 0
-
-
+# retrive csv for analyst 
 def file_check(path):
     with open(path) as data:
         csvReader = csv.DictReader(data, delimiter=',')
-        header = next(csvReader)
-        for row in  csvReader:
+        for row in csvReader:
             yield row
-
-
-for row in (file_check(data_file2)): 
-    ttlMonths = ttlMonths + 1 
-    amntOfRev = amntOfRev+ int(row["Revenue"]) 
-    #print(amntOfRev)
-    revChng = int(row["Revenue"]) - prevRevenue
-    revDiff = revDiff + [revChng]
-    prevRev = int(row["Revenue"])
-    #print(type(prevRev))
-    month0fChng = month0fChng + [row["Date"]]
-    if (revChng > greatestInc[1]):
-        greatestInc[0] = row["Date"]
-        greatestInc[1] = revChng
-    if (revChng < greatestDec[1]):
-        greatestDec[0] = row["Date"]
-        greatestDec[1] = revChng
+        return csvReader
+            
+# function to help group the votes by candidates 
+# Return data of candidates who received votes base on index (0,1,2,3,...)
+def candidates_with_votes(data,who='Candidate'):
+    candidatesVotes = {}
+    for row in file_check(data_file):
+        # Our Candidates
+        candidates = row[who]
+        #list to add voter ID values for each dictionary keys 
+        candidatesList = []
         
-def _avg(self): 
-    for row in revDiff: 
-        avg = sum(revDiff)/len(revDiff) 
-        return "Average Revenue Change: $" +str(avg)
+        #if name of candidate is a member of dictionary candidatesVotes 
+        if candidates in candidatesVotes:
+        # fill in members in the list
+            candidatesList = candidatesVotes[candidates]
+        # add ID 
+        candidatesList.append(row['Voter ID'])
+         # fill in group voter id keys by member
+        candidatesVotes[candidates] = candidatesList         
+    return candidatesVotes
 
 
-result = (
-        f"\nFinancial Analysis\n"
-        f"----------------------------\n"
-        f"Total Months: {ttlMonths}\n"
-        f"Total Revenue: {amntOfRev}\n"
-        f"{_avg(revDiff)}\n"
-        f"Greatest Increase in Revenue: {greatestInc[0]} (${greatestInc[1]})\n"
-        f"Greatest Decrease in Revenue: {greatestDec[0]} (${greatestDec[1]})\n")
-with open(r_analysis, "w") as file:
-    file.write(result)
+# The total number of votes each candidate won
+def numberOfVotesPer(candidate):
+    votesData  = candidates_with_votes(file_check(data_file))
+    candidates = votesData[candidate]
+    total = 0
+    for n in candidates:
+        total = total + 1
+    return total
 
+#The total number of votes cast 
+def totalVotes():
+    #Count all the rows
+    countV = 0
+    for row in file_check(data_file):
+        countV = countV + 1
+    return(countV)
+
+#print(type(candidates_with_votes(file_check(data_file))))
+#print(V)
+
+#The percentage of votes each candidate won    - divide the sum of each candidates votes by the total amount of votes
+def percentage_of_votes(index):
+    total = 0
+    voteCounts = {}
+    percentofVotes = {}
+    votePercentage = 0 
+    results = []
+    # assign the data of the group candidates  
+    data = candidates_with_votes(file_check(data_file))
+    for v in data.items():
+        #list of candidates votes 
+        votes = v[1]  
+        # total for all votes
+        total = total + len(votes) 
+        if v[0] not in voteCounts:
+        # assign value to key of candidates in votecounts
+            voteCounts[v[0]]= 0        
+    #gather the total amount of votes for each candidates 
+    #and assign to voteCounts variables
+            voteCounts[v[0]] = voteCounts[v[0]] + len(votes)
+    # Calculate percentage by dividing
+    for values in voteCounts:
+        counts = voteCounts.get(values)
+        votePercentage = counts / total * 100
+        percentofVotes[values] = 0
+        percentofVotes[values] = percentofVotes[values] +  votePercentage
+    #return list with name and percentage
+    #results = list(percentofVotes.items())
+    # return only values***
+    results = list(percentofVotes.values())
+    return results[index]
+
+# copy of function above but this will only return a string
+def percentageOfVotesCandidate(value):
+    total = 0
+    voteCounts = {}
+    percentofVotes = {}
+    votePercentage = 0 
+    results = []
+    # assign the data of the group candidates  
+    data = candidates_with_votes(file_check(data_file))
+    for v in data.items():
+        #list of candidates votes 
+        votes = v[1]  
+        # total for all votes
+        total = total + len(votes) 
+        if v[0] not in voteCounts:
+        # assign value to key of candidates in votecounts
+            voteCounts[v[0]]= 0        
+    #gather the total amount of votes for each candidates 
+    #and assign to voteCounts variables
+            voteCounts[v[0]] = voteCounts[v[0]] + len(votes)
+    # Calculate percentage by dividing
+    for values in voteCounts:
+        counts = voteCounts.get(values)
+        votePercentage = float(counts / total) * 100
+        percentofVotes[values] = 0
+        percentofVotes[values] = percentofVotes[values] +  votePercentage
+    results = list(percentofVotes)
+    return str(results[value])
+
+
+#The winner of the election based on popular vote. 
+#- Who received the most votes
+def winnerOfElection():
+    votesData  = candidates_with_votes(file_check(data_file))
+    mPopular = 0 
+    winner = ''
+    for person in votesData:
+        votes = votesData.get(person)
+        #popularV = len(votesData[person])
+        #print(person)
+        if(len(votes) > mPopular):
+            mPopular = len(votes)
+            return(person)     
+def main():
+    
+    print(
+          f"\nElection Results\n"
+          f"-------------------------\n"
+          f"Total Votes: {totalVotes()}\n"
+          f"-------------------------\n"
+          f" {percentageOfVotesCandidate(0)}  {percentage_of_votes(0)}  {numberOfVotesPer(percentageOfVotesCandidate(0))}\n"
+          f" {percentageOfVotesCandidate(1)}  {percentage_of_votes(1)}  {numberOfVotesPer(percentageOfVotesCandidate(1))}\n"
+          f" {percentageOfVotesCandidate(2)}  {percentage_of_votes(2)}  {numberOfVotesPer(percentageOfVotesCandidate(2))}\n"
+          f" {percentageOfVotesCandidate(3)}  {percentage_of_votes(3)}  {numberOfVotesPer(percentageOfVotesCandidate(3))}\n"
+          f"-------------------------\n"
+          f" Winner:: {winnerOfElection()}\n"
+          f"-------------------------\n")
+
+    writeout = (
+          f"\nElection Results\n"
+          f"-------------------------\n"
+          f"Total Votes: {totalVotes()}\n"
+          f"-------------------------\n"
+          f" {percentageOfVotesCandidate(0)}  {percentage_of_votes(0)}  {numberOfVotesPer(percentageOfVotesCandidate(0))}\n"
+          f" {percentageOfVotesCandidate(1)}  {percentage_of_votes(1)}  {numberOfVotesPer(percentageOfVotesCandidate(1))}\n"
+          f" {percentageOfVotesCandidate(2)}  {percentage_of_votes(2)}  {numberOfVotesPer(percentageOfVotesCandidate(2))}\n"
+          f" {percentageOfVotesCandidate(3)}  {percentage_of_votes(3)}  {numberOfVotesPer(percentageOfVotesCandidate(3))}\n"
+          f"-------------------------\n"
+          f" Winner:: {winnerOfElection()}\n"
+          f"-------------------------\n")
+    with open(r_analysis, "w") as file:
+        file.write(writeout)
+
+if __name__ == '__main__':
+    main()
